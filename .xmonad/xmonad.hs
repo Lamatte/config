6b7117a4a7b02fe11584a20ddf121f2c7f88814d
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-01-06 15:02:41 +0100

    add locker

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Config.Desktop
 import XMonad.Config.Azerty
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig (additionalKeys)
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
@@ -21,10 +22,15 @@ myConfig = azertyConfig
   , startupHook   = myStartupHook
   , focusedBorderColor = "#5e8d87"
   , normalBorderColor = "#282a2e"
-  }
+  } `additionalKeys` myKeys
 
 myStartupHook =
   -- Hack to have java guis work within xmonad
   setWMName "LG3D"
   <+> spawn "picom --config ~/.xmonad/picom.conf"
   <+> spawn "feh --bg-scale ~/.xmonad/wallpapers/blurred.jpg"
+  <+> spawn "xautolock -time 5 -locker slock"
+
+myKeys =
+  [ ((mod4Mask .|. shiftMask, xK_z), spawn "slock")
+  ]
