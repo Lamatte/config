a30158f8b26d225e12f6fe4a4a7f22e19cf5b364
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-01-06 16:02:46 +0100

    fix(): link creation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function install_resource() {
   if [ -L ~/$1 ]; then
@@ -9,9 +9,12 @@ function install_resource() {
       echo "Backuping resource ~/$1 to ~/$1.backup"
       mv ~/$1 ~/$1.backup
     fi
-    ln -s $1 ~
+    ln -s $DIR/$1 ~
   fi
 }
+
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Using configuration from directory $DIR"
 
 echo "Installing packages..."
 sudo pacman -S --needed vim picom slock xmonad feh xautolock dmenu
