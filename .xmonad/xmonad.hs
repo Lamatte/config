915c94e6d9b655e9ca5386169d21d95bf00c8ff3
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-01-07 11:38:11 +0100

    additionnal layouts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,23 +3,27 @@ import XMonad.Config.Desktop
 import XMonad.Config.Azerty
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Grid
+import XMonad.Layout.NoBorders
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+import System.IO
 
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
 
-myBar = "xmobar"
+toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
 
 myPP = xmobarPP
   { ppCurrent = xmobarColor "#429942" ""
-  . wrap "<" ">"
+  . wrap "[" "]"
   }
-
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myConfig = azertyConfig
   { terminal = "urxvt"
   , modMask  = mod4Mask
   , startupHook   = myStartupHook
+  , layoutHook   = myLayout
   , focusedBorderColor = "#5e8d87"
   , normalBorderColor = "#282a2e"
   } `additionalKeys` myAdditionalKeys `additionalKeysP` myKeys
@@ -39,3 +43,8 @@ myKeys =
   [ ("<XF86MonBrightnessUp>", spawn "~/.xmonad/brightness.sh +5")
   , ("<XF86MonBrightnessDown>", spawn "~/.xmonad/brightness.sh -5")
   ]
+
+myLayout = avoidStruts $
+  Tall 1 (10/100) (2/3)
+  ||| Grid
+  ||| Full
