2df563ac88bf48791b1152ed8c431951808a7cbb
Mathieu Chauvin <mathieu.chauvin@orange.com>
2020-03-19 08:05:11 +0100

    use cntlm instead

--- set-proxy.sh
+++ set-proxy.sh
@@ -1,8 +1,10 @@
 ping -c 1 proxypac.si.francetelecom.fr >/dev/null 2<&1
 if [ $? = 0 ]; then
   echo "Orange proxy enabled"
-  export http_proxy="http://proxypac.si.francetelecom.fr:8080"
-  export https_proxy="http://proxypac.si.francetelecom.fr:8080"
+  export http_proxy="http://localhost:3128"
+  export https_proxy="http://localhost:3128"
+  #export http_proxy="http://proxypac.si.francetelecom.fr:8080"
+  #export https_proxy="http://proxypac.si.francetelecom.fr:8080"
 else
   echo "Orange proxy disabled"
   unset http_proxy https_proxy
